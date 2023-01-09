08e48d78494b86f50bf4d778d510115afe286ad4
Ronaldy <ronaldylf@gmail.com>
2023-01-09 12:40:16 -0300

    now have onClick and official status

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import '../css/Square.css';
 
-function printId(obj) {
-    console.log(obj)    
-}
 
 export default function Square (props) {
     return(
     <>
-        <div className='square-container' onClick={() => printId(props.id_)}>
-            {props.id_}
+        <div className='square-container' onClick={props.onClick}>
+        {props.status}
         </div>
     </>
     )
