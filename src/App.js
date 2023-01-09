87da14f202efb6f44ededcf9d4e09885d364f2d6
Ronaldy <ronaldylf@gmail.com>
2023-01-09 12:34:35 -0300

    App return Game component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Table from './components/Table';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
-      <Table/>
+      <Game/>
     </div>
   );
 }
