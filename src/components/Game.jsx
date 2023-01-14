dd0f92d56507b78b026d3a9ca1720d122baf0e1c
Ronaldy <ronaldylf@gmail.com>
2023-01-14 18:47:27 -0300

    added status

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,19 +5,23 @@ import { useState } from 'react';
 export default function Game() {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [isXtime, setisXTime] = useState(true);
+    const [status, setStatus] = useState("Start some game");
 
     function handleClick(id_) {
         let new_squares = squares.slice();
         if (new_squares[id_]) return;// avoid already used square
-
         new_squares[id_] = (isXtime) ? "X" : "O";
+        const next_player = (isXtime) ? "O" : "X";
+        const status = `Next player: ${next_player}`;
+        console.log(status);
+        setStatus(status);
         setSquares(new_squares); // update squares with the new move
         setisXTime(!isXtime); // change player move
-        console.log("clicked " + String(id_));
     }
 
     return(
     <>
+    <h1 className='status'>{status}</h1>
     <Table handleClick={handleClick} squares={squares}/>
     </>
     )
