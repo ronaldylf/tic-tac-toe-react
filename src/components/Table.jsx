3e90455f9aea0ed69037abb46723a9b592fff8c4
Ronaldy <ronaldylf@gmail.com>
2023-01-19 15:04:59 -0300

    centralized class table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,25 +15,24 @@ export default function Table(props) {
     }
 
     return(
-    <>
-    <div className='board-row'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-    </div>
+    <div className='table'>
+        <div className='board-row'>
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+        </div>
 
-    <div className='board-row'>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-    </div>
+        <div className='board-row'>
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
 
-    <div className='board-row'>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+        <div className='board-row'>
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
     </div>
-
-    </> 
     )
 }
