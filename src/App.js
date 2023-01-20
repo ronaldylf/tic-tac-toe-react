a8ae4577b71bd0f369e0b006157faa7b550bd1f1
Ronaldy <ronaldylf@gmail.com>
2023-01-20 15:03:07 -0300

    added main title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Game from './components/Game';
 function App() {
   return (
     <div className="App">
+      <h1 className='main-title'> Tic Tac Toe with reactJS </h1>
       <Game/>
     </div>
   );
